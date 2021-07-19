143621128fb1b155f62d6827ec4370f934625530
NiliPilipp <86464701+NiliPilipp@users.noreply.github.com>
2021-07-19 10:10:56 +0300

    Add search bar, buttons and integrate w API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 // netlify link https://serene-hypatia-f67995.netlify.app/
+function getCurrentDate() {
+  let currentDate = document.querySelector(".date-block .current-day");
+  let currentTime = document.querySelector(".date-block .current-time");
+  let currentMonth = document.querySelector(".date-block .current-month");
+  let currentYear = document.querySelector(".date-block .current-year");
+  let now = new Date();
+  currentDate.innerHTML = getCurrentDay(now.getDay());
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  currentTime.innerHTML = now.getHours() + ":" + minutes;
+  currentMonth.innerHTML = getCurrentMonth(now.getMonth());
+  currentYear.innerHTML = now.getFullYear();
+}
+
 function displayWeather(response) {
-  let temperatureElement = document.querySelector("#IDtemperature");
+  console.log("display weather");
+  let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  let cityElement = document.querySelector("#IDcity");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let humidityElement = document.querySelector("#IDhumidity");
   humidityElement.innerHTML = response.data.main.humidity;
@@ -15,9 +32,23 @@ function displayWeather(response) {
     response.data.weather[0].description;
 }
 
-let units = "metric";
-let apiKey = "4fc9de9420224385e6f3f281435126d7";
-let newCity = "Tel Aviv";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=${units}&appid=${apiKey}`;
-console.log(`URL is ${apiURL}`);
-axios.get(apiURL).then(displayWeather);
+function getWeather(newCity) {
+  let units = "metric";
+  let apiKey = "4fc9de9420224385e6f3f281435126d7";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=${units}&appid=${apiKey}`;
+  console.log(`URL is ${apiURL}`);
+  axios.get(apiURL).then(displayWeather);
+}
+
+function getCity(event) {
+  event.preventDefault();
+  let newCity = document.querySelector("#search-bar").value.toLowerCase();
+  if (newCity.trim().length === 0) {
+    alert("Please enter a city to continue.");
+  } else {
+    getWeather(newCity);
+  }
+}
+
+let searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", getCity);
