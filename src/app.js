40d693522d4200fe9b46eb26f5bb88b65998d911
NiliPilipp <86464701+NiliPilipp@users.noreply.github.com>
2021-07-19 10:44:17 +0300

    Add weather for current location

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ function displayWeather(response) {
     response.data.weather[0].description;
 }
 
-function getWeather(newCity) {
+function getNewCityWeather(newCity) {
   let units = "metric";
   let apiKey = "4fc9de9420224385e6f3f281435126d7";
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=${units}&appid=${apiKey}`;
@@ -46,9 +46,26 @@ function getCity(event) {
   if (newCity.trim().length === 0) {
     alert("Please enter a city to continue.");
   } else {
-    getWeather(newCity);
+    getNewCityWeather(newCity);
   }
 }
+
+function getLocalWeather(response) {
+  console.log("getLocalWeather");
+  let apiKey = "4fc9de9420224385e6f3f281435126d7";
+  let units = "metric";
+  let latitude = response.coords.latitude;
+  let longitude = response.coords.longitude;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayWeather);
+}
+
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getLocalWeather);
+}
+
 function getCurrentDate() {
   let today = document.querySelector("#day");
   let currentMonth = document.querySelector("#month");
@@ -89,3 +106,6 @@ function getCurrentDate() {
 getCurrentDate();
 let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", getCity);
+
+let locationButton = document.querySelector("#current-location");
+locationButton.addEventListener("click", getLocation);
