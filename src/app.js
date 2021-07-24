0ef20cc0e99d105e7719a9791002bf8c2a0f5857
NiliPilipp <86464701+NiliPilipp@users.noreply.github.com>
2021-07-24 10:58:28 +0300

    Add temperature unit conversion

--- src/app.js
+++ src/app.js
@@ -35,20 +35,20 @@ function displayDate(now) {
   } else if (hours === 12) {
     currentTime.innerHTML = now.getHours() + ":" + minutes + " PM";
   } else if (hours > 12) {
-    console.log(`hours are ${hours}`);
     currentTime.innerHTML = now.getHours() - 12 + ":" + minutes + " PM";
   }
 }
 
 function displayWeather(response) {
-  console.log("display weather");
+  CelciusTemp = Math.round(response.data.main.temp);
+
   let iconElement = document.querySelector("#weatherIcon");
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = CelciusTemp;
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let humidityElement = document.querySelector("#IDhumidity");
@@ -60,18 +60,11 @@ function displayWeather(response) {
     response.data.weather[0].main +
     " with " +
     response.data.weather[0].description;
-  //convert to new    time zone
-  // create Date object for current location
+
+  //convert to new time zone
   localTime = new Date();
-  // convert to msec
-  // add local time zone offset
-  // get UTC time in msec
   utcTime = localTime.getTime() + localTime.getTimezoneOffset() * 60000;
-
-  // create new Date object for different city
-  // using supplied offset in seconds and converted to milliseconds
   newCityTime = new Date(utcTime + 1000 * response.data.timezone);
-
   displayDate(newCityTime);
 }
 
@@ -79,7 +72,6 @@ function getNewCityWeather(newCity) {
   let units = "metric";
   let apiKey = "4fc9de9420224385e6f3f281435126d7";
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=${units}&appid=${apiKey}`;
-  console.log(`URL is ${apiURL}`);
   axios
     .get(apiURL)
     .then(displayWeather)
@@ -101,7 +93,6 @@ function getCity(event) {
 }
 
 function getLocalWeather(response) {
-  console.log("getLocalWeather");
   let apiKey = "4fc9de9420224385e6f3f281435126d7";
   let units = "metric";
   let latitude = response.coords.latitude;
@@ -110,21 +101,39 @@ function getLocalWeather(response) {
   axios.get(apiURL).then(displayWeather);
 }
 
-function getCurrentLocation(event) {
-  event.preventDefault();
+function getCurrentData() {
   navigator.geolocation.getCurrentPosition(getLocalWeather);
   let now = new Date();
   displayDate(now);
 }
-
-function getCurrentDate() {
-  let now = new Date();
-  displayDate(now);
+function goCurrentDataHandler(event) {
+  event.preventDefault();
+  getCurrentData();
 }
 
-getCurrentDate();
+function convertFarenheit(event) {
+  event.preventDefault;
+  let convertedTemp = document.querySelector("#temperature");
+  convertedTemp.innerHTML = Math.round((CelciusTemp * 9) / 5 + 32);
+}
+
+function convertCelcius(event) {
+  event.preventDefault;
+  let convertedTemp = document.querySelector("#temperature");
+  convertedTemp.innerHTML = CelciusTemp;
+}
+
+let CelciusTemp = null;
+getCurrentData();
+
 let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", getCity);
 
 let locationButton = document.querySelector("#current-location");
-locationButton.addEventListener("click", getCurrentLocation);
+locationButton.addEventListener("click", goCurrentDataHandler);
+
+let getFarenheit = document.querySelector("#toFarenheit");
+let getCelcius = document.querySelector("#toCelcius");
+
+getFarenheit.addEventListener("click", convertFarenheit);
+getCelcius.addEventListener("click", convertCelcius);
